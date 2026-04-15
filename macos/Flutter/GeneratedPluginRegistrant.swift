7d3e70d0970bf85271bfd0c51dfeb3f140f94f46
Cristhian David Recalde <recaldecd@gmail.com>
2026-04-15 14:55:08 -0500

    Remove firebase_core and related dependencies from project files. Updated pubspec.lock, GeneratedPluginRegistrant.swift, generated_plugin_registrant.cc, and generated_plugins.cmake to reflect these changes, streamlining the project by eliminating unused plugins.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,6 @@ import FlutterMacOS
 import Foundation
 
 import cupertino_native
-import firebase_core
 import path_provider_foundation
 import printing
 import sqflite_darwin
@@ -14,7 +13,6 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   CupertinoNativePlugin.register(with: registry.registrar(forPlugin: "CupertinoNativePlugin"))
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
